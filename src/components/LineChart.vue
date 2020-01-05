b5845b52e8778b54fc5560d80141d59a62005bb0
organizm <?A1s2d3f4?>
2020-01-05 13:27:49 +0200

    feat: Line chart component. save to file.

---
+++ src/components/LineChart.vue
@@ -0,0 +1,17 @@
+<template>
+  <GChart
+    style="height: 600px"
+    type="LineChart"
+    :data="chartData"
+    :options="chartOptions"
+  />
+</template>
+
+<script>
+export default {
+  name: "LineChart",
+  props: ["chartData", "chartOptions"]
+};
+</script>
+
+<style scoped></style>
--- src/components/Recorder.vue
+++ src/components/Recorder.vue
@@ -1,55 +1,90 @@
-<template
-  ><div class="recorder__wrapper">
+<template>
+  <div class="recorder__wrapper">
     <div class="recorder__body">
-      <div class="recorder__header"><h3>Recorder</h3></div>
-      <div class="recorder__content">{{ port }}</div>
-      <GChart
-        type="LineChart"
-        :data="chart.chartData"
-        :options="chart.chartOptions"
-      />
-    </div></div
-></template>
+      <div class="recorder__header row">
+        <div class="col-1">Recorder</div>
+        <div class="col-2">
+          Pulse: <b>{{ pulse.vol }} V</b>
+        </div>
+        <div class="col-2">
+          <button @click="btnStart" class="btn btn-success">
+            {{ write ? "Stop" : "Start" }}
+          </button>
+        </div>
+        <div class="col-2">
+          <button @click="btnSave" class="btn btn-info">
+            Save
+          </button>
+        </div>
+      </div>
+      <line-chart :chart-data="chart.chartData" :chart-options="chartOptions" />
+    </div>
+  </div>
+</template>
 
 <script>
-import { reactive, onMounted, onUnmounted } from "@vue/composition-api";
+const { dialog } = require("electron").remote;
+import fs from "fs";
+import { ref, reactive, onMounted, onUnmounted } from "@vue/composition-api";
 import SerialPort from "serialport";
 const Readline = require("@serialport/parser-readline");
+import dayjs from "dayjs";
+import LineChart from "./LineChart";
 
 export default {
   name: "Recorder",
-  components: {},
+  components: { LineChart },
   props: ["port"],
   setup({ port }) {
-    let i = 20;
+    const chartOptions = {
+      title: "Company Performance"
+    };
+    let write = ref(false);
+    const pulse = reactive({
+      vol: 0
+    });
     const chart = reactive({
       chartData: [
-        ["Time", "frict"],
-        [0, 0],
-        [1, 10],
-        [2, 23],
-        [3, 17]
-      ],
-      chartOptions: {
-        chart: {
-          title: "Company Performance",
-          subtitle: "Sales, Expenses, and Profit: 2014-2017"
-        }
-      }
+        ["Time", "friction"],
+        [0, 0]
+      ]
     });
     let connection = {};
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
+
+    function btnStart() {
+      write.value = !write.value;
     }
-    setInterval(() => {
-      chart.chartData.push([i, getRandomInt(100)]);
-      i++;
-      console.log(chart.chartData);
-    }, 1000);
+    async function btnSave() {
+      // You can obviously give a direct path without use the dialog (C:/Program Files/path/myfileexample.txt)
+      console.log("Hello");
+      const { canceled, filePath } = await dialog.showSaveDialog(null);
+      if (filePath === undefined && canceled) {
+        console.log("You didn't save the file");
+        return;
+      }
+      const content = JSON.stringify({
+        chartData: chart.chartData,
+        date: dayjs().format("YYYY-MM-DD HH:mm:ss")
+      });
+      // fileName is a string that contains the path and filename created in the save file dialog.
+      fs.writeFile(`${filePath}.json`, content, err => {
+        if (err) {
+          alert("An error ocurred creating the file " + err.message);
+        }
+
+        alert("The file has been succesfully saved");
+      });
+    }
     onMounted(async () => {
       connection = new SerialPort(port);
       const parser = connection.pipe(new Readline({ delimiter: "\r\n" }));
-      parser.on("data", console.log);
+      parser.on("data", data => {
+        const point = processData(data, chart.chartData);
+        pulse.vol = point[1];
+        if (write.value) {
+          chart.chartData.push(point);
+        }
+      });
     });
     onUnmounted(() => {
       if (connection.isOpen) {
@@ -58,10 +93,20 @@ export default {
     });
 
     return {
-      chart
+      chart,
+      chartOptions,
+      pulse,
+      write,
+      btnStart,
+      btnSave
     };
   }
 };
+function processData(data, chartData) {
+  const point = data.split("/").map(parseFloat);
+  const prev = chartData[chartData.length - 1];
+  return [prev[0] + point[1], point[0]];
+}
 </script>
 
 <style scoped>
